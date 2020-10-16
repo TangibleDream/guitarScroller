8bd39a0126fcfdb0aaf9200f83334359acb0c7c3
tangibledream <darclan28@yahoo.com>
2020-10-15 22:35:31 -0500

    added a front page

--- test/guitar.test.js
+++ test/guitar.test.js
@@ -1,7 +1,17 @@
 const execSync = require('child_process').execSync;
+const exists = (el) => {
+  let result = false;
+  if (document.getElementById(el) != null) result = true;
+  return result;
+}
 
 test('README.md is not a stub', () => {
     let count = /\d+/;
-    console.log(count.exec(execSync('wc -l README.md')));
     expect(count.exec(execSync('wc -l README.md')) > 20).toBe(true);
-  });
+});
+
+test('There is a frontpage', () => {
+  let count = /\d+/;
+    expect(count.exec(execSync('wc -l index.html')) > 0).toBe(true);
+});
+
