ab7bd2489958c8017fe97fd560381c3fde94dceb
tangibledream <darclan28@yahoo.com>
2020-10-17 11:03:20 -0500

    added ruftyTufty.js sample

---
+++ ruftyTufty.js
@@ -0,0 +1,207 @@
+[
+    {
+        'note' : 'A3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'G3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'G3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'A3',   //repeat
+        'duration' : 'H'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'G3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'G3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'Q'
+    },//B Section
+        {
+        'note' : 'A3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'G3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'G3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'H'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'D3',
+        'duration' : 'Q'
+    },
+    {
+        'note' : 'Bar',
+        'duration' : null
+    },
+    {
+        'note' : 'E3',
+        'duration' : 'E'
+    },
+    {
+        'note' : 'F#3',
+        'duration' : 'E'
+    },
+
+]
