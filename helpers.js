c5d42e384665dfda797e26c92c4ebe9d33dbc6ac
tangibledream <darclan28@yahoo.com>
2020-10-28 22:06:37 -0500

    added tablature writer

--- helpers.js
+++ helpers.js
@@ -1,14 +1,33 @@
 'use strict';
 import { songArray } from './core.js'
-import { noteSelect } from './elements.js'
-import { durationSelect } from './elements.js' 
+import { noteSelect, durationSelect, g6Area, scrollerArea } from './elements.js'
+import { durationPic, g6Pic, toGuitarFraction } from './util.js'
 
 const addBreak = () => {
     let result = songArray.concat([{ 'note' : 'Bar', 'duration' : null }]);
+    let newBreak = document.createElement('td');
+    let newBreakG6 = document.createElement('img');
+    newBreakG6.src = './images/break-6S.png'
+    newBreak.innerText  = '|\r\n|\r\n|';
+    scrollerArea.appendChild(newBreak);
+    g6Area.appendChild(newBreakG6);
     return result;
 }
 const addNote = () => {
-    let result = songArray.concat([{ 'note' : noteSelect.options[noteSelect.selectedIndex].value, 'duration' : durationSelect.options[durationSelect.selectedIndex].value }]);
+    let selNote = noteSelect.options[noteSelect.selectedIndex].value;
+    let selDuration = durationSelect.options[durationSelect.selectedIndex].value;
+    let result = songArray.concat([{ 'note' : selNote, 'duration' : selDuration }]);
+    let newNote = document.createElement('td');
+    let newDuration = document.createElement("img");
+    let newG6 = document.createElement("img");
+    newG6.src = g6Pic(selNote);
+    //newG6.width = "30"
+    newDuration.src = durationPic(selDuration);
+    newDuration.width = "30"
+    newNote.innerText = `${selNote}\r\n${toGuitarFraction(selNote)}\r\n`;
+    scrollerArea.appendChild(newNote);
+    newNote.appendChild(newDuration)
+    g6Area.appendChild(newG6);
     return result
 }
 const songDump = () => {
