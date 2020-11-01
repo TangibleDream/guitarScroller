4a1b665f55dc93b8064b81d0fe106a9f49d1e8db
tangibledream <darclan28@yahoo.com>
2020-11-01 15:25:42 -0600

    added preset board

--- core.js
+++ core.js
@@ -1,6 +1,7 @@
 //'use strict';
 
 import { addBreak, addNote, songDump } from './helpers.js';
+import { durationPic, max5 } from './util.js'
 import { addBreakButton, addNoteButton, g6Area, musicArrayArea, printSongButton, removeNote, scrollerArea } from './elements.js'
 let songArray = [];
 addBreakButton.addEventListener("click", function(){
@@ -8,6 +9,22 @@ addBreakButton.addEventListener("click", function(){
 })
 addNoteButton.addEventListener("click", function(){
     songArray = addNote();
+    let psNotes = max5('note');
+    let psDuration = max5('duration');
+    let picArray = []
+    psNotes.forEach(function(value, i) {
+        document.getElementById(`psh-${i+1}`).setValue = value;
+        document.getElementById(`psh-${i+1}`).textContent = value;
+    })
+    psDuration.forEach(function(value, i) {
+        picArray[i] = document.createElement('img');
+        picArray[i].src=`${durationPic(value)}`;
+        picArray[i].width = "30"
+        let durationTitle = document.getElementById(`psdur-${i+1}`)
+        durationTitle.setValue = value;
+         if (durationTitle.firstChild) durationTitle.removeChild(durationTitle.lastChild);
+        document.getElementById(`psdur-${i+1}`).appendChild(picArray[i]);
+    })
 });
 printSongButton.addEventListener("click", function(){
     musicArrayArea.setAttribute('style', 'white-space: pre;');
